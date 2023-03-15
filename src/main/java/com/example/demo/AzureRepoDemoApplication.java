6bdab239dd86899395815baad8977dc34e1d5645
Shashwat412 <53264296+Shashwat412@users.noreply.github.com>
2023-03-15 10:51:46 +0530

    first commit

---
+++ src/main/java/com/example/demo/AzureRepoDemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AzureRepoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureRepoDemoApplication.class, args);
+	}
+	
+	@GetMapping("/demo")
+	public String retString()
+	{
+		return "DEMO REPO";
+	}
+
+}
